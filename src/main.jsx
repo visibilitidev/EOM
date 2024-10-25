13d15eddd4410d8f618605278fead64a2d1a3479
Akram-00 <aliakram9789@gmail.com>
2024-10-25 10:15:43 +0530

    preloaders removed

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Circle } from 'react-preloaders'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Circle background="white" color="yellow" time={2000} />
     <App />
   </StrictMode>,
 )
