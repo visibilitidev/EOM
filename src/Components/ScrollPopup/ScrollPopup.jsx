f5cc53e0dd5bff945c651d2785f093871ac8abad
Akram-00 <aliakram9789@gmail.com>
2024-10-30 13:53:40 +0530

    Scroll pop up corrected

--- src/Components/ScrollPopup/ScrollPopup.jsx
+++ src/Components/ScrollPopup/ScrollPopup.jsx
@@ -6,7 +6,7 @@ const ScrollPopup = () => {
   const [showPopup, setShowPopup] = useState(false);
   const [hasScrolled, setHasScrolled] = useState(false);
 
- 
+
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 200 && !hasScrolled) {
@@ -30,21 +30,23 @@ const ScrollPopup = () => {
   return (
     <>
       {showPopup && (
-        <div className="fixed inset-0 bg-blue-900 bg-opacity-80 flex items-center justify-center z-50">
-            <button
-              onClick={closePopup}
-              className="absolute top-10 right-20 text-white hover:text-gray-800 text-2xl font-bold"
-            >
-              &#10005;
-            </button>
-          <div className="relative p-6 rounded-lg  w-1/3 ">
-          
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="absolute inset-0 bg-[#000101ba] bg-opacity-80 backdrop-blur-sm"></div>
+
+          <button
+            onClick={closePopup}
+            className="absolute bottom-[5%] md:bottom-[90%] md:right-[5%] text-white hover:text-gray-800 text-2xl font-bold"
+          >
+            &#10005;
+          </button>
+          <div className="relative p-6 w-full sm:w-10/12 md:w-9/12 lg:w-5/12">
+
             <img
               src={assets.popup}
               alt="Popup"
               className="w-full h-auto rounded-md"
             />
-            
+
           </div>
         </div>
       )}
